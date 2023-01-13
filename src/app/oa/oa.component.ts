c3598056fb9c4a3e60d7a89205dff45a74785f14
abuzar <abuzarmulla456@gmail.com>
2023-01-13 17:02:43 +0530

    working styles with button

--- src/app/oa/oa.component.ts
+++ src/app/oa/oa.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 import { HttpClient } from '@angular/common/http';
 import {
   FormGroup,
@@ -27,20 +28,20 @@ export class OAComponent {
   arr: Arr = { Factors: '0', Levels: '0' };
   addForm: FormGroup;
   displayedColumns=[]
-  rows: FormArray;
-  itemForm: FormGroup;
+  rows=[];
+  flagoa=false;
+  flagnext=true;
 
   Factors=0
   Number_of_factor: string = '0';
 
+
   /**Initializing row as array of form builder*/
-  constructor(private fb: FormBuilder) {
-    this.rows = this.fb.array([]);
-  }
+  
 
   /**Adding rows to form group */
   ngOnInit() {
-    this.addForm.addControl('rows', this.rows);
+    
   }
   
   /**Getting the factor value and generating columns based on Factor value*/
@@ -54,15 +55,80 @@ export class OAComponent {
 
     for(let i=0 ; i< Number(this.Factors); i++)
     {
-      this.rows.push(this.createItemFormGroup());
+      this.rows.push({
+        Factor_name:'',
+        Level_count: '',
+        Level_value:[],
+        Level_values:''
+      });
     }
   }
-
+  
+  display(){
+    for(let i=0;i<Number(this.Factors);i++){
+      for(let j=0;j<Number(this.rows[i].Level_count);j++){
+        this.rows[i].Level_value.push({value:''})
+      }
+    }
+    console.log("level_values:")
+    console.log(this.rows)
+    this.flagoa=true;
+    this.flagnext=false;
+    $('.inputId').prop('readonly', true);
+  }
   /**used to create element of form array*/
-  createItemFormGroup(): FormGroup {
-    return this.fb.group({
-      Factor_name: null,
-      Level_values: null,
-    });
+
+
+  CreateOA(){
+    for(let i=0;i<Number(this.Factors);i++){
+      for(let j=0;j<this.rows[i].Level_value.length;j++){
+        
+        this.rows[i].Level_values+=this.rows[i].Level_value[j].value
+        this.rows[i].Level_values+=','
+      }
+      this.rows[i].Level_values=this.rows[i].Level_values.slice(0,this.rows[i].Level_values.length-1)
+  }
+  
+  console.log(this.rows)
+  }
+
+  emptylist(){
+    if(this.flagnext==true){
+      $(".inputId").val(' ');
+      this.rows=[]
+      this.onSave()
+    }
+    else if(this.flagoa==true)
+    {
+      $(".inputlevels").val(' ');
+      for(let i=0;i<Number(this.Factors);i++){
+        this.rows[i].Level_value=[] 
+      }
+      for(let i=0;i<Number(this.Factors);i++){
+        for(let j=0;j<Number(this.rows[i].Level_count);j++){
+          this.rows[i].Level_value.push({value:''})
+        }
+      }
+    }
+
+  }
+
+  backfunction(){
+    if(this.flagoa==true)
+    {
+      $(".inputlevels").val(' ');
+      for(let i=0;i<Number(this.Factors);i++){
+        this.rows[i].Level_value=[] 
+      }
+      this.flagoa=false
+      this.flagnext=true
+    }
+    else if(this.flagnext==true)
+    {
+      $(".inputId").val(' ');
+      this.rows=[]
+      this.firstScreen=true
+      this.secondScreen=false
+    }
   }
 }
